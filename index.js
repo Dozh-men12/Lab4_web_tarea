ceb331d064354199661eaf385616219c41012e6f
Oscar <ohohurtado16@gmail.com>
2023-04-25 12:13:28 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const { Socket } = require('dgram')
+const express = require('express')
+const app= express()
+
+app.use(express.static(__dirname+'/'));
+
+const http = require('http')
+const { dirname } = require('path')
+const server = http.createServer(app) 
+
+const {Server} = require('socket.io')
+const io = new Server(server)
+
+io.on('connection' , (socket) => {
+    //procedimiento 1:
+    console.log('Un usuario se ha conectado')
+}) 
+
+io.on('connection',(socket)=>{
+    //Procedimiento 2:
+    socket.on('disconnect',()=>{
+        console.log('Un usuario se ha desconectado')
+    })
+})
+
+io.on('connection',(socket)=>{
+    //Procedimiento 3:
+    socket.on('chat',(msg)=>{
+        console.log('Mensaje : ' +msg)
+    })
+})
+
+io.on('connection',(socket)=>{
+    //Procedimiento 4:
+    socket.on('chat',(msg) => {
+        io.emit('chat',msg)
+    })
+})
+
+
+app.get('/',(req,resp)=>{
+    resp.sendFile(`${__dirname}/chat_view.html`)
+})
+
+server.listen(3000,() => {
+    console.log('Servidor corriendo en http://localhost:3000')
+})
